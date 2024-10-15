6ac3a275503a597f317c14f5129ea08a481d69c7
Ivan Collado <119608773+ivncd@users.noreply.github.com>
2024-10-15 14:38:02 +0000

    Setup Ace editor and initialize program components

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,18 @@
-console.log("Funciona")
+import aceBuilds from 'https://cdn.jsdelivr.net/npm/ace-builds@1.36.2/+esm'
+
+import Compiler from './models/Compiler';
+import Instruction from './models/Instruction';
+import Interpreter from './models/Interpreter';
+
+
+/* Setting up code editor */
+const editor = aceBuilds.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/javascript");
+
+
+let code : string =  editor.getValue();
+const compiler = new Compiler(code)
+let instructions : Instruction[] = compiler.obtainInstructions()
+
+const interpreter = new Interpreter(instructions);
