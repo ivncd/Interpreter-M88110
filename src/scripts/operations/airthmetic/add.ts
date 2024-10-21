643ca05f0728cbc5596b557be2996a6131ec798b
Ivan Collado <119608773+ivncd@users.noreply.github.com>
2024-10-21 14:30:18 +0000

    Created Add operation with extension handler

---
+++ src/scripts/operations/airthmetic/add.ts
@@ -0,0 +1,34 @@
+import Operation from "../Operation";
+import Operand from "../../models/Operand";
+
+class Add implements Operation{
+    static EXTENSIONS : { [extension : string] : (context : Add) => number} =  {
+        "" : (context) => context.signed(),
+        "u" : (context) => context.unsigned()
+    };
+
+    extension : string;
+    operands : Operand[];
+    constructor(extension : string, operands : Operand[]){
+        this.extension = extension;
+        this.operands = operands;
+    }
+
+    // TODO: Make these operations
+    public unsigned() : number {
+        let value = this.operands[1].get() + this.operands[2].get();
+
+        return value
+    }
+
+    public signed() : number {
+        let value = this.operands[1].get() + this.operands[2].get();
+
+        return value
+    }
+
+    public execute() : void{
+        const func = Add.EXTENSIONS[this.extension];
+        this.operands[0].set(func(this));
+    }
+}
