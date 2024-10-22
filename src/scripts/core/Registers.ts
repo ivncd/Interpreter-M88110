2f1c1f9f7a593e07bf42f4f365b301669515dab6
Ivan Collado <119608773+ivncd@users.noreply.github.com>
2024-10-22 14:48:18 +0000

    feat: Implement Registers class

---
+++ src/scripts/core/Registers.ts
@@ -0,0 +1,33 @@
+export default class Registers{
+    // Creates a map with registers from r0 to r31
+    // r0 is 0 and the others are undefined
+    public static map = new Map<string, number | null>(
+        Array.from({ length: 32 }, (_, i) => [`r${i}`, i === 0 ? 0 : null])
+    );
+
+    public static isRegister(register : string) {
+        return Registers.map.has(register);
+    }
+
+    public static set(key: string, value : number) : void{
+        Registers.map.set(key, value);
+    }
+
+    public static get(key: string) : number{
+        let value : number | undefined | null = Registers.map.get(key);
+
+        if(value === undefined)
+            throw new ReferenceError(`The register ${key} doesnt exist`);
+        else if(value === null)
+                throw new Error(`The value of the register ${key} is null`);
+
+        return value;
+    }
+
+    // Clear all values of the maps reset it
+    public static reset() : void{
+        Registers.map = new Map<string, number | null>(
+            Array.from({ length: 32 }, (_, i) => [`r${i}`, i === 0 ? 0 : null])
+        );
+    }
+}
