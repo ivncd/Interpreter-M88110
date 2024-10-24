05bc88ff8e726640c551b36cd54f494a9668b175
Ivan Collado <119608773+ivncd@users.noreply.github.com>
2024-10-24 21:48:01 +0000

    feat: Initial test for compiler and interpreter using Add operation

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,31 +1,38 @@
 import aceBuilds from 'ace-builds/src-min-noconflict/ace';
 
 import Compiler from './core/Compiler';
-import Instruction from './models/Instruction';
 import Interpreter from './core/Interpreter';
 
-
+import Registers from './core/Registers';
 import RegistersTable from './ui/RegistersTable';
 
-/* Setting up code editor */
-function setupEditor(editorId : string) : void{
-    editor.setTheme("ace/theme/monokai");
-    editor.session.setMode("ace/mode/javascript");
-}
+const editor = aceBuilds.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/javascript");
+
 
 function setupRegisters(div : HTMLElement) : void{
     RegistersTable.build();
-    console.log(RegistersTable.getTable())
     div.appendChild(RegistersTable.getTable());
 }
 
 
-
 const divRegisters : HTMLElement = document.getElementById("registers")!;
-const buttonExecuteAll = document.getElementById("executeAll");
-
-const editor = aceBuilds.edit("editor");
-setupEditor(editor)
 setupRegisters(divRegisters);
 
-//buttonExecuteAll?.addEventListener("click", main);
+// first Test
+Registers.set("r1", 1)
+Registers.set("r2", 2)
+Registers.set("r3", 3)
+
+let code = `
+add r1, r2, r3
+add r4, r2, r3
+`
+
+const compiler = new Compiler(code);
+const interpreter = new Interpreter(compiler.getInstructions());
+interpreter.executeAll()
+
+//const buttonExecuteAll = document.getElementById("executeAll");
+//buttonExecuteAll!.addEventListener("click", main);
