1b40d29e74904fa4b92021586ffa9c10d6bad22f
Ivan Collado <119608773+ivncd@users.noreply.github.com>
2024-10-25 23:33:45 +0000

    chore: Configure build directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import path from 'path';
 export default defineConfig({
   root: 'src',
   base: '/Interpreter-M88110/',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       '/ace': path.resolve(__dirname, 'node_modules/ace-builds/src-noconflict')
