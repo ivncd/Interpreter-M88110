9393fc7aed6cb5c43b8ed3d78a13de200eb1bee6
Ivan Collado <119608773+ivncd@users.noreply.github.com>
2024-10-25 23:22:32 +0000

    feat: Add workflow for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 export default defineConfig({
   root: 'src',
+  base: '/Interpreter-M88110/',
   resolve: {
     alias: {
       '/ace': path.resolve(__dirname, 'node_modules/ace-builds/src-noconflict')
