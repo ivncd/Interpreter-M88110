52ac1dff020d23b6632913fe70d26b9f308f0dfe
Ivan Collado <119608773+ivncd@users.noreply.github.com>
2024-10-28 17:45:26 +0000

    feat: Created functions for signed/unsigned transformations

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,5 +1,20 @@
-export function to32BitHex(num : number) {
-    let hexString = (num >>> 0).toString(16);
+export function to32BitHex(num: number): string {
+    let adjustedNum = num >>> 0;
+    let hexString = adjustedNum.toString(16);
 
     return '0x' + hexString.padStart(8, '0');
+}
+
+export function hexToDecimal(hexNumber : string, signed = true) {
+    const bitLength = hexNumber.length * 4;
+    const num = parseInt(hexNumber, 16);
+
+    if (signed) {
+        const maxPositiveValue = 1 << (bitLength - 1);
+        if (num >= maxPositiveValue) {
+            return num - (1 << bitLength);
+        }
+    }
+
+    return num;
 }
