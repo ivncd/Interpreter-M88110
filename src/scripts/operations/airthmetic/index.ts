c0b3f973d2bfdbd5bb994b64a7efe38c532c238e
Ivan Collado <119608773+ivncd@users.noreply.github.com>
2024-11-13 16:20:37 +0000

    feat: Setup index.ts in operation type folders

---
+++ src/scripts/operations/airthmetic/index.ts
@@ -0,0 +1,8 @@
+export { default as Add } from "./add";
+export { default as Addu } from "./addu";
+export { default as Sub } from "./sub";
+export { default as Subu } from "./subu";
+export { default as Muls } from "./muls";
+export { default as Mulu } from "./mulu";
+export { default as Divs } from "./divs";
+export { default as Divu } from "./divu";
